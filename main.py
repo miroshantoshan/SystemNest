03a0ddfc7770a927943a34021af798b276964936
MiroshanToshan <abakumovmiron608@gmail.com>
2025-12-28 11:19:23 +0500

    Added distro logos

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import customtkinter as ctk
 import webbrowser
 import json
 import os
-
+from PIL import Image
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
-
 
 window = ctk.CTk()
 window.title("SystemNest")
@@ -14,15 +13,13 @@ window.geometry("400x550")
 window.resizable(False, False)
 window.iconbitmap("images/logo.ico")
 
-
 all_buttons = []
 
-
 def load_distros():
-
     if os.path.exists("distros.json"):
-            with open("distros.json", "r", encoding="utf-8") as f:
-                return json.load(f)
+        with open("distros.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
 
 distro_data = load_distros() 
 
@@ -31,27 +28,36 @@ def update_list(*args):
         btn.destroy()
     all_buttons.clear()
 
-
     search_text = search_var.get().lower()
     found_count = 0
-
 
     for item in distro_data:
         name = item.get("name", "???")
         link = item.get("link", "#")
+        logo_path = item.get("logo", "") # Читаем путь из ключа "logo"
 
         if search_text in name.lower():
+            # Загрузка иконки
+            icon = None
+            if logo_path and os.path.exists(logo_path):
+                try:
+                    img = Image.open(logo_path)
+                    icon = ctk.CTkImage(light_image=img, dark_image=img, size=(24, 24))
+                except:
+                    icon = None
 
             new_btn = ctk.CTkButton(
                 scroll_frame,
                 text=name,
+                image=icon,         # Установка иконки
+                compound="left",    # Картинка слева от текста
                 command=lambda l=link: webbrowser.open(l),
-                height=32,
+                height=40,
                 fg_color="#2c3e50",
                 hover_color="#033ca5",
                 anchor="w"
             )
-            new_btn.pack(fill="x", pady=1, padx=5)
+            new_btn.pack(fill="x", pady=2, padx=5)
             all_buttons.append(new_btn)
             found_count += 1
 
@@ -66,7 +72,6 @@ tabview.pack(padx=10, pady=5, fill="both", expand=True)
 
 tab_search = tabview.add("Search")
 tab_info = tabview.add("Info")
-
 
 search_var = ctk.StringVar()
 search_var.trace_add("write", update_list) 
@@ -85,7 +90,6 @@ counter_label.pack()
 scroll_frame = ctk.CTkScrollableFrame(tab_search, label_text="Results")
 scroll_frame.pack(pady=5, padx=5, fill="both", expand=True)
 
-
 ctk.CTkLabel(tab_info, text="SystemNest", font=("Arial", 16, "bold")).pack(pady=10)
 ctk.CTkLabel(tab_info, text="Compact bible\n all of systems", font=("Arial", 12)).pack()
 ctk.CTkLabel(tab_info, text='''
@@ -93,6 +97,5 @@ ctk.CTkLabel(tab_info, text='''
 Version: 0.10.0 Beta''', font=("Arial", 15)).pack()
 
 update_list()
-
 
 window.mainloop()
